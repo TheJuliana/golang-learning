2aecd502dc361202a1c774db7c3960aee4260263
TheJuliana <juliana.svetlakova@gmail.com>
2022-05-29 14:37:04 +0200

    SMI completed

--- SMI/smi.go
+++ SMI/smi.go
@@ -11,6 +11,14 @@ type Rectangle struct {
 	Height float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
